26fd8e8c6bc259989b4e0d63356696c53262b840
spaldingra <spaldingra@vcu.edu>
2020-09-21 23:54:51 -0400

    fixed UI layout not showing issue, restructure java code file, added comments for code blocks

--- app/src/main/java/com/example/pillreminderapp/MainActivity.java
+++ app/src/main/java/com/example/pillreminderapp/MainActivity.java
@@ -14,40 +14,43 @@ import android.widget.ImageButton;
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-    Button aNotification;
 
-    /*
-
-
-
-
-
-
+    /* layout class */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        aNotification = findViewById(R.id.bt_notification);
-        aNotification.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String message = "Take your meds";
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(
-                        MainActivity.this
-                )
-                        .setContentTitle("New Notification")
-                        .setContentText(message)
-                        .setAutoCancel(true);
-                Intent intent = new Intent(MainActivity.this,NotificationActivity.class);
-            }
-        });
 
-        /*************************************************
+    }
 
+    /* settings button class */
     public void goToSettingsActivity (View view){
         Intent intent = new Intent (this, activity_settings.class);
         startActivity(intent);
     }
-     */
+
+
+    /* notifications code block */
+
+    /*
+
+    Button aNotification;
+
+    aNotification = findViewById(R.id.bt_notification);
+    aNotification.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String message = "Take your meds";
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                    MainActivity.this
+            )
+                    .setContentTitle("New Notification")
+                    .setContentText(message)
+                    .setAutoCancel(true);
+            Intent intent = new Intent(MainActivity.this,NotificationActivity.class);
+        }
+    });
+
+    */
 
 }
