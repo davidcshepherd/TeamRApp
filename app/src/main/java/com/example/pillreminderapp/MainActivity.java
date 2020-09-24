8bec46e9b3d554f72d20e624dd74cf516d6b6927
shaikhhi880 <shaikhhi@mymail.vcu.edu>
2020-09-24 16:50:13 -0400

    Notification
    The last thing I tried to do notifcation didn't work :(, I will try again tommorow,

--- app/src/main/java/com/example/pillreminderapp/MainActivity.java
+++ app/src/main/java/com/example/pillreminderapp/MainActivity.java
@@ -20,28 +20,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        aNotification = findViewById(R.id.bt_notification);
-        aNotification.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String message = "Take your meds";
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(
-                        MainActivity.this
-                )
-                        .setContentTitle("New Notification")
-                        .setContentText(message)
-                        .setAutoCancel(true);
-                Intent intent = new Intent(MainActivity.this, NotificationActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                intent.putExtra("message",message);
-                PendingIntent pendingIntent= PendingIntent.getActivity(MainActivity.this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
-                builder.setContentIntent(pendingIntent);
-                NotificationManager notificationManager =(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-                notificationManager.notify(0,builder.build());
-
-            }
-        });
-
     }
 
         public void goToSettingsActivity (View view){
