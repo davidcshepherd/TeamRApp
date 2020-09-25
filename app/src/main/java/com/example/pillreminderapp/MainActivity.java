4e18854b4074d0b15cd2bfb5bc286e930b35cc7d
shaikhhi880 <shaikhhi@mymail.vcu.edu>
2020-09-25 11:19:41 -0400

    Some more notification struggles

--- app/src/main/java/com/example/pillreminderapp/MainActivity.java
+++ app/src/main/java/com/example/pillreminderapp/MainActivity.java
@@ -3,17 +3,39 @@ package com.example.pillreminderapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-    Button aNotification;
 
+
+    NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+            .setSmallIcon(R.drawable.rx_icon)
+            .setContentTitle("Daily Reminder")
+            .setContentText("Take your pills")
+            .setStyle( new NotificationCompat.BigTextStyle())
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        //    CharSequence name = getString(R.string.channel_name);
+        //    String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+         //   NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+       //     channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+         //   notificationManager.createNotificationChannel(channel);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
