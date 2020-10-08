36a4dfd1cf3399270595b6cd262fc4d652c8b5c8
araRina <inserttheemailyouusehere@gmail.com>
2020-10-08 17:23:09 -0400

    Make a medication class test

--- app/src/test/java/com/example/pillreminderapp/TestMedClass.java
+++ app/src/test/java/com/example/pillreminderapp/TestMedClass.java
@@ -8,17 +8,14 @@ import java.sql.Time;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleUnitTest {
+public class TestMedClass {
     @Test
-    public void addition_isCorrect() {
+    public void timeWorks() {
         Time time = new Time(10, 10, 10);
 
         Medication medication = new Medication("Yeah", time);
+
+        assertTrue(medication.time_taken == time);
 
     }
 }
